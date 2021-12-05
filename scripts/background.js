ca902f1e9b8e88d30f59e9ffdcf68908da197be0
Krishan-Patel <61567619+Krishan-Patel@users.noreply.github.com>
2021-12-04 20:23:39 -0500

    fixed double install reset issue

--- scripts/background.js
+++ scripts/background.js
@@ -4,14 +4,25 @@ import { updateData } from "./Data Functions.js";
 chrome.runtime.onInstalled.addListener(function(object) {
     console.log(object)
     if(object.reason === 'install') {
-      chrome.storage.sync.set({'background' : "https://images.unsplash.com/photo-1523130979271-d463aac0e7e9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"});
-      chrome.storage.sync.set({bookmarks: []});
-      chrome.storage.sync.set({leagues : ["https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard"]});
+      chrome.storage.sync.get('background', (result) => {
+        if (result.background == undefined) {
+          chrome.storage.sync.set({'background' : "https://images.unsplash.com/photo-1523130979271-d463aac0e7e9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2550&q=80"});
+        } 
+      })
+      chrome.storage.sync.get('bookmarks', (result) => { 
+        if (result.bookmarks == undefined) { 
+          chrome.storage.sync.set({bookmarks: []});
+        }
+      });
+      chrome.storage.sync.get('leagues', (result) => {
+        if (result.leagues == undefined) {
+         chrome.storage.sync.set({leagues : ["https://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard"]});
+        }
+      })
       chrome.tabs.create({url:chrome.extension.getURL("../settings.html")}, function (tab) { 			
         console.log("New tab launched with local file"); 		
       });
     }
-    
   });
 
 // creates a chrome alarm event that fires every 30 minutes 
